8cf31168f3e78d806ece0321f065d37854b6c640
Pavel Glukhov <pavel.glukhov@t-systems.com>
2022-05-13 17:40:29 +0300

    [master] added test for not a number values

--- 01-intro/1-sum/index.test.js
+++ 01-intro/1-sum/index.test.js
@@ -3,5 +3,6 @@ import sum from './index.js';
 describe('intro/sum', () => {
   it('should return sum of numbers', () => {
     expect(sum(1, 1)).toEqual(2);
+    expect(sum('zero', 1)).toEqual(NaN);
   });
 });
